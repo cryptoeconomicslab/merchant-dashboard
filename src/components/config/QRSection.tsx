d1e2e3eaae953c2f7b4ab0dcab0da7c0fa742bc3
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-03-22 18:07:42 +0900

    use env

--- src/components/config/QRSection.tsx
+++ src/components/config/QRSection.tsx
@@ -4,8 +4,8 @@ import { ChamberWallet } from '@layer2/wallet'
 
 const QRSection = ({ walletRef }: { walletRef: ChamberWallet }) => {
   const address = walletRef.getAddress()
-  // TODO: use env for url
-  const value = `http://localhost:8080/transfer?address=${address}&amount=0`
+  const value = `${process.env.USER_WALLET_URL ||
+    'http://localhost:8080'}/transfer?address=${address}&amount=0`
   return (
     <div>
       <QRCode value={value} renderAs="svg" />
